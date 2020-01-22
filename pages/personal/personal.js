7a658353910a8e972bb614fe9b58faf26f4e6e61
WaynePeng <pengwayne@outlook.com>
2020-01-22 22:16:15 +0800

    修复storage不存在的问题

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,63 +1,59 @@
-// pages/personal/personal.js
-var app = getApp();
-var Bmob = require('../../utils/bmob.js');
+var app = getApp()
+var Bmob = require('../../utils/bmob.js')
 Page({
   data: {
-    userinfo: JSON.parse(
-      wx.getStorageSync('bmob')
-    ),
+    userinfo: {}
   },
-  onLoad: function (options) {
-    console.log("s")
-    if(this.data.userinfo.sessionToken){
+  onLoad: function(options) {
+    console.log('s')
+    if(wx.getStorageSync('bmob')) {
+      this.data.userinfo = JSON.parse(wx.getStorageSync('bmob'))
+    }
+    if (this.data.userinfo.sessionToken) {
       console.log('cc')
       wx.redirectTo({
-        url: '../login/login',
+        url: '../login/login'
       })
     }
-
   },
   // 页面初始化 options为页面跳转所带来的参数
 
-  onShow: function () {
+  onShow: function() {
     // 页面显示.
-
   },
 
-  listmybook: function () {
+  listmybook: function() {
     // 我的预订
     wx.redirectTo({
-      url: '../mybook/mybook',
+      url: '../mybook/mybook'
     })
   },
-  listchangeperinfo: function () {
+  listchangeperinfo: function() {
     // 我的预订
     wx.redirectTo({
-      url: '../changeperinfo/changeperinfo',
+      url: '../changeperinfo/changeperinfo'
     })
   },
-  exitlogin: function (e) {
+  exitlogin: function(e) {
     wx.showModal({
       title: '提示',
       content: '是否确认退出',
-      success: function (res) {
+      success: function(res) {
         if (res.confirm) {
           // console.log('用户点击确定')
-          wx.removeStorageSync('username');
+          wx.removeStorageSync('username')
           //页面跳转
           wx.redirectTo({
-            url: '../login/login',
+            url: '../login/login'
           })
         } else if (res.cancel) {
           console.log('用户点击取消')
         }
       }
     })
-
-
   },
 
-  getScanning: function () {
+  getScanning: function() {
     app.getScanning()
   }
 })
