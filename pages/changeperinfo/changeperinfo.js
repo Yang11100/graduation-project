c96dcd941306002e1ce0389723d629d07bb0f295
WaynePeng <pengwayne@outlook.com>
2020-02-20 14:45:50 +0800

    显示修改昵称

--- pages/changeperinfo/changeperinfo.js
+++ pages/changeperinfo/changeperinfo.js
@@ -6,26 +6,29 @@ Page({
     username: null, //当前用户的用户名
     Nickname: null, //昵称
     mark: null, //积分
-
-    changeNickName: null, //修改后的用户名
-
+    isChangeNickName: false,
+    changeNickName: null //修改后的用户名
   },
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       objectId: JSON.parse(wx.getStorageSync('bmob')).objectId,
       username: JSON.parse(wx.getStorageSync('bmob')).username,
       Nickname: JSON.parse(wx.getStorageSync('bmob')).Nickname,
-      mark: JSON.parse(wx.getStorageSync('bmob')).mark,
+      mark: JSON.parse(wx.getStorageSync('bmob')).mark
     })
     console.log('Id', this.data.objectId)
   },
-  NicknameInput: function (e) {
+  NicknameInput: function(e) {
     this.setData({
       changeNickName: e.detail.value
     })
     console.log(e)
-
+  },
+  // 修改昵称
+  $onChangeNickName() {
+    this.setData({
+      isChangeNickName: !this.data.isChangeNickName
+    })
   }
-
 })
